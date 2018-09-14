d7a9883492ab5a87c4b2573637a7de78b01de383
EMi <emi.bohm@gmail.com>
2018-09-14 16:34:25 +0200

    hamamamam

---
+++ New folder/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ev;
+            Console.WriteLine("Írjon be egy évszámot!");
+            ev = int.Parse(Console.ReadLine());
+            if (ev % 4 == 0)
+            {
+                Console.WriteLine("Szökőév!");
+            }
+            else
+            {
+                Console.WriteLine("Nem szökőéve!");
+            }
+            Console.ReadLine();
+            return;
+        }
+    }
+}
---
+++ New folder/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int februar = 28;
+            //31 day
+            int[] x=new int[] { 1, 3, 5, 7, 8, 10, 12 };
+            //30 day
+            int[] y=new int[] { 4, 6, 9, 11 };
+            int result=0;
+            Console.WriteLine("Írjon be egy évszámot!");
+            int ev=int.Parse(Console.ReadLine());
+            if (ev % 4 == 0)
+            {
+                 februar = 29;
+            }
+            Console.WriteLine("Írjon be egy hónapot szám formátumban!");
+            int honapszam = int.Parse(Console.ReadLine());
+            if (x.Contains(honapszam))
+            {
+                result = 31;
+            }
+            else if (y.Contains(honapszam))//honapszam=amit beolvastunk
+            {
+                result = 30;
+            }
+            else if (honapszam == 2)
+            {
+                result = februar;
+            }
+            else {
+                Console.WriteLine("Nincs ilyen hónap!");
+                Console.ReadLine();
+                return;
+            }
+            Console.WriteLine("A(z) " + ev + " év " + honapszam + ". hónapja " + result + " napos.");
+            Console.ReadLine();
+
+        }
+    }
+}
